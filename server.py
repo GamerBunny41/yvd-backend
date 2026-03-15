421762ae23dec09f894ac78e293201a4a6514227
Bunny <gamerbunny982@gmail.com>
2026-03-15 18:12:04 +0530

    Update server.py

--- server.py
+++ server.py
@@ -202,7 +202,9 @@ def _do_download(task_id, url, fmt, q_num, cookies, cookie_fmt, out_base):
         elif fmt == "audio":
             opts["format"] = "bestaudio[ext=m4a]/bestaudio/best"
         else:
-            opts["format"] = "best[ext=mp4]/best"
+            opts["format"] = (
+                f"bestvideo[height<={q_num}][ext=mp4]+bestaudio[ext=m4a]"
+                f"/best[height<={q_num}][ext=mp4]/best[height<={q_num}]/best"
             )
             opts["merge_output_format"] = "mp4"
 
