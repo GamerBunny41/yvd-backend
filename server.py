ad6b1224e9fc05a50eb454ecfd3b430bbef4e6e0
Bunny <gamerbunny982@gmail.com>
2026-03-15 18:59:19 +0530

    Update server.py

--- server.py
+++ server.py
@@ -31,8 +31,7 @@ def build_ydl_opts(cookies=None, cookie_format="header"):
         "http_headers": {"User-Agent": USER_AGENT},
         "extractor_args": {
             "youtube": {
-                "player_client": ["android", "web"],
-                "player_skip": ["configs"],
+                "player_client": ["ios"],
             }
         },
     }
